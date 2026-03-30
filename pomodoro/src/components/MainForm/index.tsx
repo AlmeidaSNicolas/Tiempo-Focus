87009456cba9273f6445094f6f7eec08ef29763a
Nicolas <nicolassalesalmeida@gmail.com>
2026-03-30 19:27:19 -0300

    "Config form"

--- pomodoro/src/components/MainForm/index.tsx
+++ pomodoro/src/components/MainForm/index.tsx
@@ -2,13 +2,33 @@ import { DefaultInput } from "../DefaultInput"
 import { DefaultButton } from "../DefaultButton"
 import { Cycles } from "../Cycles"
 import { PlayCircleIcon } from "lucide-react"
+import { useRef } from "react"
+
 
 export function MainForm(){
+
+    const taskNameInput = useRef<HTMLInputElement>(null);
+
+    //const [taskName, setTaskName] = useState(''); achei que assim seria melhor, mas pra validação do input, o ideal é usar o useRef, pois ele tem acesso direto ao elemento do DOM, e não precisa ficar renderizando o componente toda vez que o valor do input mudar, como acontece com o useState.
+
+    function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>){
+        event.preventDefault();
+
+        console.log('deu certo');
+    }
     return(
     <> 
-          < form className='form' action=''>
+          <form onSubmit={handleCreateNewTask} className='form' action=''>
                         <div className='formRow'>
-                            <DefaultInput labelText= 'Tarefa' id='input' type='text' placeholder='Digite Algo'/>
+                            <DefaultInput 
+                            labelText= 'Tarefa' 
+                            id='input'
+                            type='text' 
+                            placeholder='Digite o nome da tarefa'
+                            ref={taskNameInput}
+                            //value={taskName} 
+                            //onChange={(e) => setTaskName(e.target.value)}
+                            />
                         </div>
         
                         <div className='formRow'>
