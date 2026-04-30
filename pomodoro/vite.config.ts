38ce483d9e453fc5cc12a3b2fb4b6eae8d92859a
Nicolas <nicolassalesalmeida@gmail.com>
2026-04-30 14:42:25 -0300

    "error vite :()"

--- pomodoro/vite.config.ts
+++ pomodoro/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tiempo-focus',
   server: {
     host: true,      // Permite que o servidor seja acessado fora do container
     port: 4173,      // Mantém a porta que já está em uso no projeto
