e60e096a4c182ec9df657073c90fc0a3ce1f056f
Nicolas <nicolassalesalmeida@gmail.com>
2026-04-30 15:27:52 -0300

    "Removendo config do github pages"

--- pomodoro/vite.config.ts
+++ pomodoro/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tiempo-focus',
   server: {
     host: true,      // Permite que o servidor seja acessado fora do container
     port: 4173,      // Mantém a porta que já está em uso no projeto
